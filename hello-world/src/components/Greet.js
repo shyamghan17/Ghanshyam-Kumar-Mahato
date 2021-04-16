07981bb28a86b5207e37f893aecc6d5f8d5ec1b7
shyamghan17 <48532181+shyamghan17@users.noreply.github.com>
2021-04-16 14:56:47 +0545

    .

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
-function Greet() {
-    return <h1>Hello Ghanshyam</h1>
-}
+// function Greet() {
+//     return <h1>Hello Ghanshyam</h1>
+// }
+ const Greet = () => <h1> Hello Ghanshyam</h1>
+
+
+export default Greet
